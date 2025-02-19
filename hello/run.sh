dc5af3b3bd2766a44e72cdb0e74f4b36a2975eea
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-19 01:55:53 -0800

    hello agda

--- hello/run.sh
+++ hello/run.sh
@@ -2,4 +2,7 @@
 
 STDLIB_PATH=/home/diego/git/misc/agda-stdlib
 echo "Using stdlib path: $STDLIB_PATH"
-agda --compile --library-file=$STDLIB_PATH/standard-library.agda-lib hello.agda
+echo "Library file contents:"
+cat $STDLIB_PATH/standard-library.agda-lib
+echo "---"
+agda -v 2 --compile --library-file=$STDLIB_PATH/standard-library.agda-lib hello.agda
