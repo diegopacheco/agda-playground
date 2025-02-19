ef5e283dcdcb371d12eaa520809bdaf9ed88e169
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-19 02:17:35 -0800

    hello agda - 100% working

--- hello/hello.agda
+++ hello/hello.agda
@@ -1,5 +1,7 @@
+{-# OPTIONS --guardedness #-}
 module hello where
 
 open import IO
 
+main : Main
 main = run (putStrLn "Hello, World!")
--- hello/run.sh
+++ hello/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-agda --compile hello.agda
+agda --guardedness --compile hello.agda
+./hello
+
+rm -rf MAlonzo
+rm hello
+rm hello.agdai
